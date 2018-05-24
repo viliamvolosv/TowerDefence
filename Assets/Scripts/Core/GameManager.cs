ff18a4459b12153e088cf7541e52c70a68f1dba9
ViliamVolosV <ViliamVolosV@gmail.com>
2018-05-24 12:05:07 +0300

    Add ui, and gold count

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -3,41 +3,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
+    public LevelSettings LevelSettings;
 
-	public LevelSettings LevelSettings;
+    private int _currentGold = 0;
 
-	private int _currentGold = 0;
+    public int CurrentGold
+    {
+        get { return _currentGold; }
+        set
+        {
+            _currentGold = value;
+            OnGoldAmountChange();
+        }
+    }
 
-	public enum GameStateEnum
-	{
-		Build,
-		Wave,
-		End
-	}
+    public UnityEvent OnGoldAmountChangeEvent;
 
-	private GameStateEnum _gameState;
-	public GameStateEnum GameState
-	{
-		get { return _gameState; }
-		set
-		{
-			_gameState = value;
-			OnChageGameState();
-		}
-	}
 
-	void Start () {
-		Assert.IsNotNull(LevelSettings," You need to setup level settings");
-		_currentGold = LevelSettings.StartGold;
-		GameState = GameStateEnum.Build;
+    public enum GameStateEnum
+    {
+        Build,
+        Wave,
+        End
+    }
 
-	}
+    private GameStateEnum _gameState;
 
-	private void  OnChageGameState()
-	{
-		
-	}
+    public GameStateEnum GameState
+    {
+        get { return _gameState; }
+        set
+        {
+            _gameState = value;
+            OnChageGameState();
+        }
+    }
+
+    void Start()
+    {
+        Assert.IsNotNull(LevelSettings, " You need to setup level settings");
+        CurrentGold = LevelSettings.StartGold;
+        GameState = GameStateEnum.Build;
+    }
+
+    private void OnChageGameState()
+    {
+    }
+
+    private void OnGoldAmountChange()
+    {
+        if (OnGoldAmountChangeEvent !=null)
+        {
+            OnGoldAmountChangeEvent.Invoke();
+        }
+    }
 }
---
+++ Assets/Scripts/UI/LevelUIBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class LevelUIBehavior : MonoBehaviour
+{
+
+	public Text GoldCountText;
+	public GameManager GameManager;
+
+	// Use this for initialization
+	void Start () {
+		Assert.IsNotNull(GoldCountText);
+		Assert.IsNotNull(GameManager);
+
+		GameManager.OnGoldAmountChangeEvent.AddListener(OnGoldAmountChangeEvent);
+	}
+
+	void OnGoldAmountChangeEvent()
+	{
+		GoldCountText.text = "Gold: " + GameManager.CurrentGold;
+	}
+
+	private void OnDestroy()
+	{
+		GameManager.OnGoldAmountChangeEvent.RemoveListener(OnGoldAmountChangeEvent);
+	}
+}
