a785272b64386da7846e3e7a5d03f5a42ccfb19c
ViliamVolosV <ViliamVolosV@gmail.com>
2018-05-25 11:09:51 +0300

    Create grid for towers;

--- Assets/Scripts/Core/PlacementGrid.cs
+++ Assets/Scripts/Core/PlacementGrid.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Core.Utilities;
+using TowerDefense.UI.HUD;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 public class PlacementGrid : MonoBehaviour
 {
+    /// <summary>
+    /// Prefab used to visualise the grid
+    /// </summary>
+    public PlacementTile placementTilePrefab;
+
     public IntVector2 Dimensions;
+
+    /// <summary>
+    /// Array of <see cref="PlacementTile"/>s
+    /// </summary>
+    PlacementTile[,] m_Tiles;
 
 
     /// <summary>
@@ -14,6 +25,12 @@ public class PlacementGrid : MonoBehaviour
     /// </summary>
     [Tooltip("The size of the edge of one grid cell for this area. Should match the physical grid size of towers")]
     public float gridSize = 1;
+
+
+    private void Start()
+    {
+        SetUpGrid();
+    }
 
     /// <summary>
     /// Set collider's size and center
@@ -26,6 +43,58 @@ public class PlacementGrid : MonoBehaviour
 
         // Collider origin is our bottom-left corner
         myCollider.center = size * 0.5f;
+    }
+
+    /// <summary>
+    /// Instantiates Tile Objects to visualise the grid and sets up the <see cref="m_AvailableCells" />
+    /// </summary>
+    protected void SetUpGrid()
+    {
+        PlacementTile tileToUse;
+        tileToUse = placementTilePrefab;
+
+
+        if (tileToUse != null)
+        {
+            // Create a container that will hold the cells.
+            var tilesParent = new GameObject("Container");
+            tilesParent.transform.parent = transform;
+            tilesParent.transform.localPosition = Vector3.zero;
+            tilesParent.transform.localRotation = Quaternion.identity;
+            m_Tiles = new PlacementTile[Dimensions.x, Dimensions.y];
+
+            for (int y = 0; y < Dimensions.y; y++)
+            {
+                for (int x = 0; x < Dimensions.x; x++)
+                {
+                    Vector3 targetPos = GridToWorld(new IntVector2(x, y), new IntVector2(1, 1));
+                    targetPos.y += 0.01f;
+                    PlacementTile newTile = Instantiate(tileToUse);
+                    newTile.transform.parent = tilesParent.transform;
+                    newTile.transform.position = targetPos;
+                    newTile.transform.localRotation = Quaternion.identity;
+
+                    m_Tiles[x, y] = newTile;
+                    newTile.SetState(PlacementTileState.Empty);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns the world coordinates corresponding to a grid location.
+    /// </summary>
+    /// <param name="gridPosition">The coordinate in grid space</param>
+    /// <param name="sizeOffset"><see cref="IntVector2"/> indicating size of object to center.</param>
+    /// <returns>Vector3 containing world coordinates for specified grid cell.</returns>
+    public Vector3 GridToWorld(IntVector2 gridPosition, IntVector2 sizeOffset)
+    {
+        // Calculate scaled local position
+        Vector3 localPos =
+            new Vector3(gridPosition.x + (sizeOffset.x * 0.5f), 0, gridPosition.y + (sizeOffset.y * 0.5f)) *
+            gridSize;
+
+        return transform.TransformPoint(localPos);
     }
 
 
